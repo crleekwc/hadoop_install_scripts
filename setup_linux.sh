8d617d31993f7453106c993848e9b58137150e66
Christopher Lee <crleekwc@gmail.com>
2019-03-23 20:34:56 +0000

    upload setup_linux.sh

---
+++ setup_linux.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install build-essential ssh lzop git rsync curl
+sudo apt-get -y install python-dev python-setuptools
+sudo apt-get -y install libcurl4-openssl-dev
+sudo easy_install pip
+sudo pip install virtualenv virtualenvwrapper python-dateutil
