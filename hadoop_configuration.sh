1f30e1b9d06e00044e5ce7baa4afc1e50e738b49
Christopher Lee <crleekwc@gmail.com>
2019-03-24 13:44:10 +0000

    add hadoop_configuration.sh

---
+++ hadoop_configuration.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "# The JAVA implementation to use" >> /srv/hadoop/etc/hadoop/hadoop-env.sh
+echo "export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt" >> /srv/hadoop/etc/hadoop/hadoop-env.sh
