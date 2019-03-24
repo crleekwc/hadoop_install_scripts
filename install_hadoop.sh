5ae670d3112caa532260a92118d7b2d812a5e7f2
Christopher Lee <crleekwc@gmail.com>
2019-03-24 14:31:52 +0000

    update install_hadoop.sh

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -O http://mirror.reverse.net/pub/apache/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz
 tar -xzf hadoop-3.1.2.tar.gz
 sudo mv hadoop-3.1.2 /srv/
 sudo chown -R hadoop:hadoop /srv/hadoop-3.1.2
