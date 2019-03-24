30d926137e6fe92bd4a5aa48572d744a1cae60c6
Christopher Lee <crleekwc@gmail.com>
2019-03-24 13:59:17 +0000

    modified values in hadoop_configuration.sh and setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -3,7 +3,7 @@ echo "# Set the Hadoop-related Environment variables" >> /home/hadoop/.bashrc
 echo "export HADOOP_HOME=/srv/hadoop" >> /home/hadoop/.bashrc
 echo 'export PATH=$PATH:$HADOOP_HOME/bin' >> /home/hadoop/.bashrc
 echo "# Set the JAVA Home" >> /home/hadoop/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt" >> /home/hadoop/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /home/hadoop/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /home/hadoop/.bashrc
 echo 'export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar' >> /home/hadoop/.bashrc
 echo "# Set the hadoop related environment variables" >> ~/.bash_aliases
@@ -11,7 +11,7 @@ echo "export HADOOP_HOME=/srv/hadoop" >> ~/.bash_aliases
 echo 'export HADOOP_STREAMING=$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.1.2.jar' >> ~/.bash_aliases
 echo 'export PATH=$PATH:$HADOOP_HOME/bin' >> ~/.bash_aliases
 echo "# Set the JAVA_HOME" >> ~/.bash_aliases
-echo "export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt" >> ~/.bash_aliases
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.bash_aliases
 echo "# Helpful Aliases" >> ~/.bash_aliases
 echo 'alias ..="cd .."' >> ~/.bash_aliases
 echo 'alias ...="cd ../.."' >> ~/.bash_aliases
