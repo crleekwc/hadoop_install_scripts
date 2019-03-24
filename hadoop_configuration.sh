75fceff99933c682ec9c8eedf8ca090ccd2d60d2
Christopher Lee <crleekwc@gmail.com>
2019-03-24 14:44:22 +0000

    update hadoop_configuration.sh

--- hadoop_configuration.sh
+++ hadoop_configuration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "# The JAVA implementation to use" >> /srv/hadoop/etc/hadoop/hadoop-env.sh
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /srv/hadoop/etc/hadoop/hadoop-env.sh
-/bin/cp -a configuration/* /srv/hadoop/etc/hadoop/
+/bin/cp -a configuration_files/* /srv/hadoop/etc/hadoop/
