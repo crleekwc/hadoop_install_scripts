840681cee3b7546d5b2d6a2a00a59a8e103ac4d3
Christopher Lee <crleekwc@gmail.com>
2019-03-27 23:10:59 +0000

    updated install_spark.sh to add more environmental variables

--- install_spark.sh
+++ install_spark.sh
@@ -6,3 +6,5 @@ ln -s /srv/spark-2.4.0 /srv/spark
 
 echo "export SPARK_HOME=/srv/spark" >> /home/hadoop/.bashrc
 echo 'export PATH=$SPARK_HOME/bin:$PATH' >> /home/hadoop/.bashrc
+echo 'export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop' >> /home/hadoop/.bashrc
+echo 'export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native:$LD_LIBRARY_PATH' >> /home/hadoop/.bashrc
